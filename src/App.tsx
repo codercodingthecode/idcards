3d7f77266bf5411b3c2085638645ea086a09185b
Andy Lavor <andersonlavor@gmail.com>
2022-11-01 06:38:19 -0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useReducer, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import {Card} from "./Components/Card";
 import {getIds, IDCard, SortableKeys} from "./Utils/Api";
@@ -7,44 +7,36 @@ import {Header} from "./Components/Header";
 
 export type searchByType = 'name.first' | 'name.last' | 'email';
 function App() {
-    // const [idCards, setIdCards] = useReducer((state: IDCard[], action: { type: 'add' | 'update', payload: IDCard | IDCard[] }) => {
-    //     switch (action.type) {
-    //         case 'add':
-    //             return [...state, ...(Array.isArray(action.payload) ? action.payload : [action.payload)];
-    //         case 'update':
-    //             return state.map((idCard, index) => {
-    //                 return idCard;
-    //     })
-    //     }
-    // }, []);
     const [idCards, setIdCards] = useState<IDCard[]>([]);
     const [sortKey, setSortKey] = useState<SortableKeys>('name.first');
     const [sortOrder, setSortOrder] = useState<'asc' | 'desc'>('asc');
     const [searchBy, setSearchBy] = useState<searchByType>('name.first');
     const [searchTerm, setSearchTerm] = useState<string>('');
 
-
     useEffect(() => {
         return () => {
             getIds().then(res => setIdCards(orderBy(res, sortKey, sortOrder)));
-            // getIds().then(res => setIdCards({type: 'add', payload: orderBy(res, sortKey, sortOrder)}));
         }
     },[])
 
     useEffect(() => {
         const sortedCards = orderBy(idCards, sortKey, sortOrder);
         setIdCards(sortedCards);
-        // setIdCards({type: 'update', payload: sortedCards});
     }, [sortKey, sortOrder]);
 
 
     const updateCard = (uuid: string, key: string, value: string) => {
         const updatedCards = idCards.find(card => card.login.uuid === uuid);
         if (updatedCards) {
-            // @ts-ignore
-            updatedCards[key] = value;
-            setIdCards([...idCards]);
-            // setIdCards({type: 'update', payload: idCards});
+            const x = key.split('.').reduce((acc, curr, index, arr) => {
+                if (index === arr.length - 1) {
+                    // @ts-ignore
+                    acc[curr] = value;
+                }
+                    // @ts-ignore
+                    return acc[curr];
+            }
+            , updatedCards);
         }
     }
 
@@ -59,7 +51,7 @@ function App() {
             <div className="cards">
                 {
                     idCards.filter((a => {
-                        return get(a, searchBy).toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase());
+                        return get(a, searchBy).toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase())
                     })).map((idCard) => {
                     return (
                         <Card key={idCard.login.uuid} {...idCard} updateCard={updateCard}/>
